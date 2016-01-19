2286c6e08216d5194ec0750e8509e748ea3e9910
Jose A. Rivera <jarrpa@redhat.com>
2016-01-18 18:44:33 -0600

    Update installation repos, release scripts, and VM deployment.

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPEC="scripts/storage-ha.spec"
+SPEC="scripts/storhaug.spec"
 
 pushd `git rev-parse --show-toplevel`
 
@@ -15,21 +15,21 @@ sed -i "s/\\(define major_version \\).*/\\1${MAJ}/" ${SPEC}
 sed -i "s/\\(define minor_version \\).*/\\1${MIN}/" ${SPEC}
 sed -i "s/\\(define release \\)[^%]*\\(.*\\)/\\1${RELEASE}\\2/" ${SPEC}
 
-LOG=$(git log --format="* %cd %aN <%aE> - ${VERSION}-${RELEASE}%n%b" --date=local -1 ${RANGE} | sed -r 's/[0-9]+:[0-9]+:[0-9]+ //')
+LOG=$(git log --format="* %cd %aN <%aE> - ${VERSION}-${RELEASE}%n%b%n%n" --date=local -1 ${RANGE} | sed -r 's/[0-9]+:[0-9]+:[0-9]+ //')
 
 sed -i "/\%changelog/a ${LOG//$'\n'/\\n}" ${SPEC}
 
 vim ${SPEC}
 
-tar -czvf storage-ha-${VERSION}.tar.gz --transform='s/^src/storage-ha/' src/*
+tar -czvf storhaug-${VERSION}.tar.gz --transform='s/^src/storhaug/' src/*
 
 rpmbuild -bs -D 'rhel 6' -D "_topdir `pwd`" -D "_sourcedir ." -D "dist .el6.centos" ${SPEC}
 rpmbuild -bs -D 'rhel 7' -D "_topdir `pwd`" -D "_sourcedir ." -D "dist .el7.centos" ${SPEC}
-rm -f storage-ha-${VERSION}.tar.gz
+rm -f storhaug-${VERSION}.tar.gz
 
 rm -rf repo/*/*
-mock -r epel-6-x86_64 --resultdir repo/el6/ SRPMS/storage-ha-${VERSION}-${RELEASE}.el6.centos.src.rpm
-mock -r epel-7-x86_64 --resultdir repo/el7/ SRPMS/storage-ha-${VERSION}-${RELEASE}.el7.centos.src.rpm
+mock -r epel-6-x86_64 --resultdir repo/el6/ SRPMS/storhaug-${VERSION}-${RELEASE}.el6.centos.src.rpm
+mock -r epel-7-x86_64 --resultdir repo/el7/ SRPMS/storhaug-${VERSION}-${RELEASE}.el7.centos.src.rpm
 createrepo repo/el6/
 createrepo repo/el7/
 
