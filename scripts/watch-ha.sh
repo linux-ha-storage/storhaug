37fd36b18a0365c3d241920ea5d3fd96182d7524
Jose A. Rivera <jarrpa@redhat.com>
2016-09-27 20:50:52 -0700

    Minor enhancements.
     - Minor whitespace, logic, and code cleanups to CTDB RA.
     - Make things friendlier to being used offline.
     - More output and timeout from watch-ha.sh.

--- scripts/watch-ha.sh
+++ scripts/watch-ha.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 CTDB_STATUS=""
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+TIMEOUT="${DIR}/timeout3.sh"
 
 if [ -e /etc/ctdb/nodes ]; then
-  CTDB_STATUS="echo; ctdb status;"
+  CTDB_STATUS="echo; ${TIMEOUT} ctdb status && echo && ${TIMEOUT} ctdb getcapabilities;"
 fi
 
-watch -n1 "pcs status; ${CTDB_STATUS}"
+watch -n1 "echo 'hostname: '`hostname`; echo; ${TIMEOUT} pcs status; ${CTDB_STATUS}"
